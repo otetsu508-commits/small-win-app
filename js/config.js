db8d69d0fee3f40076e0d83fefe57c21ef8b040f
tetsuya <otetsu508@gmail.com>
2026-07-08 09:15:23 +0900

    Fix: Supabase client initialization error

--- js/config.js
+++ js/config.js
@@ -3,4 +3,5 @@ const SUPABASE_URL = 'https://ggogpixsrqqhypgykofh.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_Ce6kKIraUgoWsCtzP48Icw_dgEVOu6p';
 
 // Supabaseクライアント初期化
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = supabaseClient;
